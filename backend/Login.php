49708722104d958c365ed18717eed53a56812085
Alexiane Chartrand <alex.sp_6@yahoo.ca>
2025-10-21 21:02:34 -0400

    push one

--- backend/Login.php
+++ backend/Login.php
@@ -8,9 +8,6 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type");
-    
-  
-
 
     $action = new LoginAction();
     $data = $action->execute(); 
